62061014f8109c145b27c74c13e2431d04a2e345
Serguei Sokol <sokol@insa-toulouse.fr>
2021-11-23 10:49:01 +0100

    updated g3mclass to v0.17.8
    Signed-off-by: Serguei Sokol <sokol@insa-toulouse.fr>

--- download/g3mclass/win/install.bat
+++ download/g3mclass/win/install.bat
@@ -3,9 +3,9 @@ rem create a launching script on user's Desktop
 
 rem author: Serguei Sokol
 @echo on
-rem v0.17.7
-set url="https://drive.google.com/uc?export=download&id=1c29CXniyLcpH5XN05jrebl6gtsFLpBfB"
-
+rem v0.17.7 set url="https://drive.google.com/uc?export=download&id=1c29CXniyLcpH5XN05jrebl6gtsFLpBfB"
+rem v0.17.8
+set url="https://drive.google.com/uc?export=download&id=1hgp9wNoo34XLP1SZYJv8WGwbmH5E1Oor"
 for /F "tokens=*" %%g in ('where python') do set pexe=%%g
 if "%pexe%"=="" (@echo Python3 was not found on this system. && pause && exit)
 for /F "tokens=*" %%g in ('%pexe% -m site --user-base') do set pbase=%%g
