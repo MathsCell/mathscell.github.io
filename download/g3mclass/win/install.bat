67adb3fc2834734ab5c4fac258f2c7d0db6d43a7
Serguei Sokol <sokol@insa-toulouse.fr>
2022-03-22 11:18:19 +0100

    updated bat v1.0

--- download/g3mclass/win/install.bat
+++ download/g3mclass/win/install.bat
@@ -3,14 +3,7 @@ rem create a launching script on user's Desktop
 
 rem author: Serguei Sokol
 @echo on
-rem v0.17.7 set url="https://drive.google.com/uc?export=download&id=1c29CXniyLcpH5XN05jrebl6gtsFLpBfB"
-rem v0.17.8 set url="https://drive.google.com/uc?export=download&id=1hgp9wNoo34XLP1SZYJv8WGwbmH5E1Oor"
-rem v0.17.9 set url="https://drive.google.com/uc?export=download&id=1xjZTyAxVc5MnAXco5oTzO20ZQ_zQ9MWp"
-rem v0.17.10 set url="https://drive.google.com/uc?export=download&id=13c0MUGEoeIVaGaL_y8Jwifjx6uGR2FBA"
-rem v0.17.11 set url="https://drive.google.com/uc?export=download&id=1tSpcdIFuI3vn9JwxXvFpeVAJC291czl6"
-rem v0.17.12
-set url="https://drive.google.com/uc?export=download&id=15rsIrTqB-nV3eLAwVOpTEkyebScJ_qSI"
-
+url=g3mclass
 for /F "tokens=*" %%g in ('where python') do set pexe=%%g
 if "%pexe%"=="" (@echo Python3 was not found on this system. && pause && exit)
 for /F "tokens=*" %%g in ('%pexe% -m site --user-base') do set pbase=%%g
